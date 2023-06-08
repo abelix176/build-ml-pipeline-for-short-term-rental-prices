8f8315bdeab56696aed41616c34a03ba506e4d9a
Felix Abrecht <felix.abrecht@gmail.com>
2023-06-08 16:15:07 +0200

    Update config and add geolocation

--- src/basic_cleaning/run.py
+++ src/basic_cleaning/run.py
@@ -31,6 +31,10 @@ def go(args):
     # Convert last_review to datetime
     df['last_review'] = pd.to_datetime(df['last_review'])
 
+    logger.info("Drop data for wrong geolocation")
+    idx = df['longitude'].between(-74.25, -73.50) & df['latitude'].between(40.5, 41.2)
+    df = df[idx].copy()
+
     logger.info("Save file")
     filename = "clean_sample.csv"
     df.to_csv(filename, index=False)
